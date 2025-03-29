e2dd73d0542d586f6f77380a2c418fc687c56f51
Timotei Sandru <timotei.sandru2022@gmail.com>
2025-03-29 14:00:38 +0200

    Update group selection (working)
    Step 4 (first part)

--- Machine_Learn/s_part.py
+++ Machine_Learn/s_part.py
@@ -5,125 +5,123 @@ import tkinter as tk
 import pandas as pd
 import random
 import time
+import f_part as gauss
+class k_medoid():
 
-global k_med_x, k_med_y, k
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Gaussian Dataset Generator")
 
-def build(fig, canvas):
-    points_list = []
-    num_groups = 5
-
-    dataset = pd.read_csv("dataset.txt", sep="\s+", names=["x", "y", "regiunepip3"])
-
-    colors = ['red', 'blue', 'green', 'purple', 'orange', 'cyan', 'magenta', 'yellow', 'black', 'brown']
-
-    # Clear the previous plot
-    fig.clf()
-    ax = fig.add_subplot(111)
-
-    # Plot the points
-    #for i, color in enumerate(colors[:num_groups]):
-    #    group_points = [(x, y) for x, y, group in points_list if group == i]
-    #    xs, ys = zip(*group_points)
-    #    ax.scatter(xs, ys, color=color, s=5, label=f'Group {i}')
-
-    ax.scatter(dataset["x"], dataset["y"], c="blue", s=10, label="Puncte")
-
-    ax.set_xlabel('X Coordinate')
-    ax.set_ylabel('Y Coordinate')
-    ax.set_title('Gaussian Distributed Points')
-    ax.legend()
-    ax.grid(True)
+        # Matplotlib Figure and Canvas
+        self.fig = plt.Figure(figsize=(5, 4), dpi=100)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.get_tk_widget().grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 
-    global k_med_x, k_med_y, k
-    k_med_x = []
-    k_med_y = []
-    dist_tmp = []
-    dataset_choice = []
+        self.dataset = pd.read_csv("dataset.txt", sep="\s+", names=["x", "y", "regiunepip3"])
+        self.k = "K = N\A"
+
+        self.k_med_x = []
+        self.k_med_y = []
+        self.dist_tmp = []
+        self.dataset_choice = []
+
+        self.fig.clf()
+        self.ax = self.fig.add_subplot(111)
+
+    def build(self):
+        self.fig.clf()
+        self.ax = self.fig.add_subplot(111)
+
+        self.k_med_x.clear()
+        self.k_med_y.clear()
+        self.dataset_choice.clear()
+
+        with open("dataset_choice.txt", "w") as f:
+            print("Hello")
+
+        self.ax.scatter(self.dataset["x"], self.dataset["y"], c="blue", s=10, label="Puncte")
+
+        self.ax.set_xlabel('X Coordinate')
+        self.ax.set_ylabel('Y Coordinate')
+        self.ax.set_title('Gaussian Distributed Points')
+        self.ax.legend()
+        self.ax.grid(True)
+        
+        for i in range(self.k):
+            rnd_term = random.randrange(0, 10000, 1)
+            dtx1 = self.dataset["x"][rnd_term]
+            dty1 = self.dataset["y"][rnd_term]
+
+            self.k_med_x.append(dtx1)
+            self.k_med_y.append(dty1)
+
+        self.ax.scatter(self.k_med_x, self.k_med_y, c="red", s=100, marker='x')
+        print("medoid list x", self.k_med_x)
+
+        for i in range(len(self.dataset)):
+            for j in range(len(self.k_med_x)):
+                dist = abs(self.k_med_x[j] - self.dataset["x"][i]) + abs(self.k_med_y[j] - self.dataset["y"][i])
+                self.dist_tmp.append(dist)
+
+            f_med = self.dist_tmp.index(min(self.dist_tmp))
+            self.dataset_choice.append(f_med) # 10000
+            self.dist_tmp.clear()
     
-    k = random.randrange(2, 10, 1)
-    # k = 3
-    print("K = ", k)
-    for i in range(k):
-        rnd_term = random.randrange(0, 10000, 1)
-        dtx1 = dataset["x"][rnd_term]
-        dty1 = dataset["y"][rnd_term]
+        with open("dataset_choice.txt", "w") as f:
+            for k in self.dataset_choice:
+                f.write(f"{k}\n")
 
-        k_med_x.append(dtx1)
-        k_med_y.append(dty1)
-
-        #print("dt1", dt1, type(dt1))
-        #print("dataset ", dataset["x"])
-        ax.scatter(dtx1, dty1, c="red", s=100, marker='x')
-    
-    print("medoid list x", k_med_x)
-
-    for i in range(len(dataset)):
-        for j in range(len(k_med_x)):
-            dist = abs(k_med_x[j] - dataset["x"][i]) + abs(k_med_y[j] - dataset["y"][i])
-            #print("kmedx", k_med_x[j], "dataset x", dataset["x"][i], "Manhattan Distance", dist)
-            dist_tmp.append(dist)
-            #print("Manhattan Distance Medoid List", dist_tmp)
-
-        #print("min", min(dist_tmp))
-        f_med = dist_tmp.index(min(dist_tmp))
-        #print("Choss value", f_med)
-        dataset_choice.append(f_med) # 10000
-        #print("dataset choice", dataset_choice)
-        dist_tmp.clear()
-
-    with open("dataset_choice.txt", "w") as f:
-        for k in dataset_choice:
-            f.write(f"{k}\n")
-
-    canvas.draw()
-
-def group(fig, canvas):
-    global k_med_x, k_med_y, k
-
-    print("medoid list x", k_med_x)
-
-    dataset = pd.read_csv("dataset.txt", sep="\s+", names=["x", "y", "regiunepip3"])
-
-    dataset_choice = pd.read_csv("dataset_choice.txt", sep="\s+", names=["choice"])
-
-    colors = ['blue', 'green', 'purple', 'orange', 'cyan', 'magenta', 'yellow', 'black', 'brown', 'red']
-
-    # Clear the previous plot
-    fig.clf()
-    ax = fig.add_subplot(111)
-
-    for i in range(len(dataset)): # 10 000
-        ax.scatter(dataset["x"][i], dataset["y"][i], c=colors[dataset_choice["choice"][i]], s=10, label="Puncte")
-
-    for i in range(len(k_med_x)):
-        ax.scatter(k_med_x[i], k_med_y[i], c="red", s=100, marker='x')
-
-    canvas.draw()
+        self.canvas.draw()
 
 
-def run_sgui():
-    def on_generate():
-        build(fig, canvas)
+    def group(self):
+        dataset_choice = pd.read_csv("dataset_choice.txt", sep="\s+", names=["choice"])
 
-    def on_group():
-        group(fig, canvas)
+        colors = ['blue', 'green', 'purple', 'orange', 'cyan', 'magenta', 'yellow', 'black', 'brown', 'red']
 
-    root = tk.Tk()
-    root.title("Gaussian Dataset Generator")
+        self.fig.clf()
+        self.ax = self.fig.add_subplot(111)
+        tmp_l_x = []
+        tmp_l_y = []
 
-    # Matplotlib Figure and Canvas
-    fig = plt.Figure(figsize=(5, 4), dpi=100)
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.get_tk_widget().grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        for i in range(len(self.k_med_x)):   # i = 0 --> 6   (for medoid = 7)
+            tmp_l_x = []
+            tmp_l_y = []
+            for j in range(len(self.dataset_choice)):  # j --> 10 000
+                if self.dataset_choice[j] == i:  # if index of 10 000 == k medoid(0--->6)
+                    tmp_l_x.append(self.dataset["x"][j])
+                    tmp_l_y.append(self.dataset["y"][j])
+            self.ax.scatter(tmp_l_x, tmp_l_y, c=colors[i], s=10, label=f"Group {i}")
 
-    # Generate button
-    generate_btn = tk.Button(root, text="Generate", command=on_generate)
-    generate_btn.grid(row=3, column=0, columnspan=2, pady=10)
+        for i in range(len(self.k_med_x)):
+            self.ax.scatter(self.k_med_x[i], self.k_med_y[i], c="red", s=100, marker='x')
 
-    group_btn = tk.Button(root, text="Group", command=on_group)
-    group_btn.grid(row=3, column=1, columnspan=2, padx=10)
+        self.canvas.draw()
 
-    root.mainloop()
+    def k_gen(self):
+        self.k = random.randrange(2, 10, 1)
+        print(self.k)
 
-run_sgui()
+        self.k_label.config(text=str(self.k))
+
+    def run_sgui(self):
+        generate_k = tk.Button(self.root, text="K Generator", command=self.k_gen)
+        generate_k.grid(row=3, column=0, pady=10)
+
+        self.k_label = tk.Label(self.root, text=str(self.k))
+        self.k_label.grid(row=4, column=0)
+
+        generate_btn = tk.Button(self.root, text="Show plot",  command=self.build)
+        generate_btn.grid(row=3, column=1, pady=10)
+
+        group_btn = tk.Button(self.root, text="Group", command=self.group)
+        group_btn.grid(row=3, column=2, padx=10)
+
+        an = tk.Button(self.root, text="Regenerate", command=gauss.run_gui)
+        an.grid(row=1, column=0, padx=10)
+
+        self.root.mainloop()
+
+myobj = k_medoid()
+myobj.run_sgui()
